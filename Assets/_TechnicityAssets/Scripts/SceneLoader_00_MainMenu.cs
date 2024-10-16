d472635366033e9b98b96f76207f849ef144976c
LiannaActivate <liannamittz@gmail.com>
2024-10-17 02:01:31 +0800

    New loading Screen - functional
    Functional loading screen
    Changed skybox
    fixed buttons for game menu

---
+++ Assets/_TechnicityAssets/Scripts/SceneLoader_00_MainMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject LoaderUI;
+    public Slider progressSlider;
+
+    private void Update()
+    {
+        // Automatically start loading if the LoaderUI is active.
+        if (LoaderUI.activeSelf && !loadingStarted)
+        {
+            LoadScene(2); // Automatically load the scene at index 2, which is "02_MainMenu Scene" in your build settings.
+        }
+    }
+
+    private bool loadingStarted = false;
+
+    public void LoadScene(int index)
+    {
+        if (!loadingStarted)
+        {
+            loadingStarted = true;
+            StartCoroutine(LoadScene_Coroutine(index));
+        }
+    }
+
+    private IEnumerator LoadScene_Coroutine(int index)
+    {
+        progressSlider.value = 0;
+        LoaderUI.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+        float progress = 0;
+
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            progressSlider.value = progress;
+            if (progress >= 0.9f)
+            {
+                progressSlider.value = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
